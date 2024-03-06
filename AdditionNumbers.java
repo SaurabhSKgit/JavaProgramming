fc0b5a89cec0de0a6b57fc65841432c712842a37
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-03-06 14:56:02 +0530

    Add files via upload
    This code will accept 2 integers form user at runtime and perform addition.

---
+++ AdditionNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class AdditionNumbers{
+public static void main(String []ar){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter First number -");
+int iNo1=sc.nextInt();
+System.out.println("enter Second number -");
+int iNo2=sc.nextInt();
+int  iAns=iNo1+iNo2;
+
+System.out.println("ans is "+iAns);
+}
+}
