48eed4ca570fbd34595cde8453a846927272bd5a
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-03-22 14:19:31 +0530

    Add files via upload
    This program prints
    a  
    a b  
    a b c  
    a b c d  
    a b c d e

---
+++ Pattern3.java
@@ -0,0 +1,15 @@
+class Pattern3{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+        char ch='a';
+            for(int j=1;j<=i;j++){
+
+            System.out.print(ch++ +" ");
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+}
