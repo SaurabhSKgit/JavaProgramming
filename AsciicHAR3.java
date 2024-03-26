28887e8a4a2505b6930fa955cb128c851ce8cbf2
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-03-27 00:54:55 +0530

    Add files via upload

---
+++ AsciicHAR3.java
@@ -0,0 +1,11 @@
+class AsciicHAR3{
+public static void main(String[] args) 
+        {
+        int val=65;
+        for(char ch='A';ch<='Z';ch++)
+        {
+                System.out.print((int)ch+ "->"+ch+" ");
+                System.out.println();
+        }
+        }
+}
