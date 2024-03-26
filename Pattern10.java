e39c8d9c85e461f799b536b79370f328e060e43a
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-03-26 22:20:11 +0530

    Add files via upload
    Prints pattern like this : 
       *
       * *
      * * *
     * * * *
    * * * * *

---
+++ Pattern10.java
@@ -0,0 +1,29 @@
+/*
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+
+*/
+
+ public class Pattern10{
+
+    public static void main(String[] args) {
+      int rows = 5;
+  
+      for (int i = 1; i <= rows; i++) {
+        // Print leading spaces
+        for (int j = 1; j <= rows - i; j++) {
+          System.out.print(" ");
+        }
+  
+        // Print stars
+        for (int k = 1; k <= i; k++) {
+          System.out.print("* ");
+        }
+        System.out.println(); // Move to the next line after each row
+      }
+    }
+  }
+  
