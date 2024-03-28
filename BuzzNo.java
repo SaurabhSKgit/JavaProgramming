fad287676275a70a65962e379a4b1d489e93c6d9
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-03-28 22:50:28 +0530

    Add files via upload
    A number is said to be Buzz if it ends with 7 or is divisible by 7.

---
+++ BuzzNo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class BuzzNo{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number :");
+		int num=sc.nextInt();
+
+        if((num%7==0)||(num%10==7))
+        {
+            System.out.println(num+ " is buzz number");
+        }
+        else{
+            System.out.println(num+" is not a buzz number");
+        }
+    }
+}
