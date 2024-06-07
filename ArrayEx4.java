c78379c572a8fe084c0469bf53ccf01abecb8986
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-06-07 23:38:40 +0530

    Add files via upload
    Accepts string input from user and stores it in array

---
+++ ArrayEx4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayEx4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String []names=new String[5];
+
+        for(int i=0;i<names.length;i++)
+        {
+            System.out.print("enter a name "+(i+1)+": ");
+            names[i]=sc.next();
+        }
+        System.out.println(Arrays.toString(names));
+    }
+}
