b10c08214e5b865cf6b5a420b459d01195e81674
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-06-12 09:02:12 +0530

    Java String Splitter
    This Java code demonstrates how to split a string into words using the split() method.
    Input : "hi how are u"
    Output : [hi, how, are, u]

---
+++ SplitString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class SplitString {
+    public static void main(String[] args) {
+        String sen="hi how are u";
+        String [] words=sen.split(" ");
+        System.out.println(Arrays.toString(words));
+    }
+}
