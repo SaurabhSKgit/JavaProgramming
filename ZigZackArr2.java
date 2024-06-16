8b038ada7f99fece41be77e3979f94fa5809676c
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-06-16 23:44:27 +0530

    zig-zag Array
    This Java program merges two arrays in a zig-zag manner. Given two arrays a and b, it interleaves their elements, starting with the elements from the second array b, and then the first array a. The resulting array contains all the elements from both input arrays in the specified order. If the arrays have different lengths, it continues to interleave until the longer array is exhausted.

---
+++ ZigZackArr2.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class ZigZackArr2 {
+    public static void main(String[] args) {
+        int []b={1,2,3};
+        int []a={4,5,6,7,8,9};
+
+        int max=((a.length)>(b.length))?a.length:b.length;
+        int c[]=new int[(a.length)+(b.length)];
+
+        int indx=0;
+        for(int i=0;i<max;i++)
+        {
+            if(i<b.length)
+            c[indx++]=b[i];
+            c[indx++]=a[i];
+        }
+        System.out.println(Arrays.toString(c));
+    }    
+}
