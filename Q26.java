1d464433675fa804db961d774632ecbc7bd738c0
SaurabhSKgit <101864932+SaurabhSKgit@users.noreply.github.com>
2024-06-26 00:02:29 +0530

    sum of all elements at even indices in an array.
    Ip : a={6,5,4,3,7,8,0}
    Op : 17

---
+++ Q26.java
@@ -0,0 +1,19 @@
+//Find sum of all elements at even indices in an array.
+//Ip : a={6,5,4,3,7,8,0}
+//Op : 17
+class Q26{
+    public static void main(String[] args) {
+        int arr[]={6,5,4,3,7,8,0};
+
+        int ans=0;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            if(i%2==0)
+            {
+                ans=ans+arr[i];
+            }
+        }
+        System.out.println("addition at even index :"+ans);
+    }
+}
